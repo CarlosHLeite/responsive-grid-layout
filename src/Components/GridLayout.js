13878bc501d5dc29322d736679b6e453dbb783c8
CarlosHLeite <carlosleiteh@gmail.com>
2022-04-04 23:48:48 -0300

    final grid

--- src/Components/GridLayout.js
+++ src/Components/GridLayout.js
@@ -6,107 +6,83 @@ import hat4 from '../img/hat-4.jpg';
 import hat5 from '../img/hat-5.jpg';
 import hat6 from '../img/hat-6.jpg';
 import hat7 from '../img/hat-7.jpg';
+import '../Styles/GridLayout/GridLayout.css'
 
 function GridLayout() {
   return (
     <main>
         <div className="container">
             <div className="product-grid">
-                <div className="card">
+                <div className="card stacked featured">
                     <img src={hat1} alt="" className='card__img'/>
                     <div className="card__content">
-                       <h2 className="card__title"></h2> 
-                       <p className="card__price"></p>
-                       <p className="card__description"></p>
+                       <h2 className="card__title">Lorem Ipsum</h2> 
+                       <p className="card__price">R$325</p>
+                       <p className="card__description">Lorem Ipsum dolor</p>
                     </div>
                 </div>
-            </div>
 
-        </div>
-
-        <div className="container">
-            <div className="product-grid">
-                <div className="card">
+                <div className="card stacked">
                     <img src={hat2} alt="" className='card__img'/>
                     <div className="card__content">
-                       <h2 className="card__title"></h2> 
-                       <p className="card__price"></p>
-                       <p className="card__description"></p>
+                       <h2 className="card__title">Lorem Ipsum</h2> 
+                       <p className="card__price">R$325</p>
+                       <p className="card__description">Lorem Ipsum dolor</p>
                     </div>
                 </div>
-            </div>
 
-        </div>
-
-        <div className="container">
-            <div className="product-grid">
-                <div className="card">
+                <div className="card stacked">
                     <img src={hat3} alt="" className='card__img'/>
                     <div className="card__content">
-                       <h2 className="card__title"></h2> 
-                       <p className="card__price"></p>
-                       <p className="card__description"></p>
+                       <h2 className="card__title">Lorem Ipsum</h2> 
+                       <p className="card__price">R$325</p>
+                       <p className="card__description">Lorem Ipsum dolor</p>
                     </div>
                 </div>
-            </div>
 
-        </div>
-
-        <div className="container">
-            <div className="product-grid">
-                <div className="card">
+                <div className="card stacked">
                     <img src={hat4} alt="" className='card__img'/>
                     <div className="card__content">
-                       <h2 className="card__title"></h2> 
-                       <p className="card__price"></p>
-                       <p className="card__description"></p>
+                       <h2 className="card__title">Lorem Ipsum</h2> 
+                       <p className="card__price">R$325</p>
+                       <p className="card__description">Lorem Ipsum dolor</p>
                     </div>
                 </div>
-            </div>
 
-        </div>
-
-        <div className="container">
-            <div className="product-grid">
-                <div className="card">
+                <div className="card stacked">
                     <img src={hat5} alt="" className='card__img'/>
                     <div className="card__content">
-                       <h2 className="card__title"></h2> 
-                       <p className="card__price"></p>
-                       <p className="card__description"></p>
+                       <h2 className="card__title">Lorem Ipsum</h2> 
+                       <p className="card__price">R$325</p>
+                       <p className="card__description">Lorem Ipsum dolor</p>
                     </div>
                 </div>
-            </div>
 
-        </div>
-
-        <div className="container">
-            <div className="product-grid">
-                <div className="card">
+                <div className="card stacked">
                     <img src={hat6} alt="" className='card__img'/>
                     <div className="card__content">
-                       <h2 className="card__title"></h2> 
-                       <p className="card__price"></p>
-                       <p className="card__description"></p>
+                       <h2 className="card__title">Lorem Ipsum</h2> 
+                       <p className="card__price">R$325</p>
+                       <p className="card__description">Lorem Ipsum dolor</p>
                     </div>
                 </div>
-            </div>
 
-        </div>
-
-        <div className="container">
-            <div className="product-grid">
-                <div className="card">
+                <div className="card stacked">
                     <img src={hat7} alt="" className='card__img'/>
                     <div className="card__content">
-                       <h2 className="card__title"></h2> 
-                       <p className="card__price"></p>
-                       <p className="card__description"></p>
+                       <h2 className="card__title">Lorem Ipsum</h2> 
+                       <p className="card__price">R$325</p>
+                       <p className="card__description">Lorem Ipsum dolor</p>
                     </div>
                 </div>
+
             </div>
 
         </div>
+
+       
+
+        
     </main>
   )
 }
